e29a5859f4dd081cc6496876cae9f97dfea0d5aa
Emil Berglund <emil.berglund@live.no>
2025-01-27 08:48:59 +0100

    Added XML comments

--- HIOF.V2025.Arbeidskrav1/BookStore/Book.cs
+++ HIOF.V2025.Arbeidskrav1/BookStore/Book.cs
@@ -5,14 +5,42 @@ using System.Threading.Tasks;
 
 namespace HIOF.V2025.Arbeidskrav1.BookStore
 {
+    /// <summary>
+    /// Represents a book in the bookstore
+    /// </summary>
     public class Book
     {
+        /// <summary>
+        /// The title of the book
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// The name of the author of the book
+        /// </summary>
         public string AuthorName { get; set; }
+        /// <summary>
+        /// The ISBN number of the book
+        /// </summary>
         public string Isbn { get; set; }
+        /// <summary>
+        /// The price of the book
+        /// </summary>
         public double Price { get; set; }
+        /// <summary>
+        /// The quantity of the book
+        /// </summary>
         public double Quantity { get; set; }
 
+
+        /// <summary>
+        /// Creates a new instance of book
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="authorName"></param>
+        /// <param name="isbn"></param>
+        /// <param name="price"></param>
+        /// <param name="quantity"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public Book(string title, string authorName, string isbn, double price, double quantity)
         {
             Title = title ?? throw new ArgumentNullException(nameof(title));
@@ -21,6 +49,10 @@ namespace HIOF.V2025.Arbeidskrav1.BookStore
             Price = price;
             Quantity = quantity;
         }
+
+        /// <summary>
+        /// Returns a string representation of the book
+        /// </summary>
         public override string ToString()
         {
             return $"Title: {Title}, Author: {AuthorName}, ISBN: {Isbn}, Price: {Price}, Quantity: {Quantity}";
--- HIOF.V2025.Arbeidskrav1/obj/Debug/net9.0/HIOF.V2025.Arbeidskrav1.AssemblyInfo.cs
+++ HIOF.V2025.Arbeidskrav1/obj/Debug/net9.0/HIOF.V2025.Arbeidskrav1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5bc4440663745bfdabcb1f06fa939ecd756c495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0c49c211a967cbafef41d446835567307deda7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
