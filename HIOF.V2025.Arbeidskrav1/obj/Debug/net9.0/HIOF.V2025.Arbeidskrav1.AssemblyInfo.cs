3d81d827595e9d6542f9bf12d5b2ae4bc13d37d5
Emil Berglund <emil.berglund@live.no>
2025-02-05 17:11:25 +0100

    Wrote further on guidelines

--- HIOF.V2025.Arbeidskrav1/obj/Debug/net9.0/HIOF.V2025.Arbeidskrav1.AssemblyInfo.cs
+++ HIOF.V2025.Arbeidskrav1/obj/Debug/net9.0/HIOF.V2025.Arbeidskrav1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf074524ba36ed6bd4437d8a62c83d9d4823c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370588f4e6c9d3ec24d2fd9e5ff65e699f3519a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
