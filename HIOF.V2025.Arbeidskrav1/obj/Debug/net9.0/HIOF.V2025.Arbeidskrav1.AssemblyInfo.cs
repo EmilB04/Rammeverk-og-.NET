846b0b13c19a8e7ee59b39259bb2ccdb1f449367
Emil Berglund <emil.berglund@live.no>
2025-02-07 12:36:02 +0100

    Working tests and main method. -MacOS

--- HIOF.V2025.Arbeidskrav1/obj/Debug/net9.0/HIOF.V2025.Arbeidskrav1.AssemblyInfo.cs
+++ HIOF.V2025.Arbeidskrav1/obj/Debug/net9.0/HIOF.V2025.Arbeidskrav1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc8af65286521d2fd094753234423afc24c97c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd8ce26a60f85d960cc69f60b87e9a245cdec9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HIOF.V2025.Arbeidskrav1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
